44c5dd3c3e7a7ab4ad85620a50142e2dcdeb5955
Johanni Brea <jbrea@users.noreply.github.com>
2018-08-11 23:28:19 +0200

    fix cliffwalking

--- src/cliffwalking.jl
+++ src/cliffwalking.jl
@@ -1,8 +1,8 @@
 # Cliff walking (Sutton & Barto)
-function CliffWalkingMDP(; offset = 0., 
-                           goalreward = 0.,
-                           cliffreward = -100.,
-                           stepreward = -1.)
+function CliffWalking(; offset = 0., 
+                        goalreward = 0.,
+                        cliffreward = -100.,
+                        stepreward = -1.)
     ns = 4*12; na = 4
     mdp = MDP(ns, na, init = "deterministic")
     mdp.isterminal[45] = 1
@@ -16,8 +16,8 @@ function CliffWalkingMDP(; offset = 0.,
                a == 2 && s > 44 || # right wall
                a == 3 && s == 1 # bottom wall (only relevant for initial state)
                 mdp.trans_probs[a, s] = SparseVector(ns, [s], [1.]) 
-            elseif mod(s, 2) == 1 && a == 3 && s != 2 && s != 44 ||
-                    s == 1 && a == 2 # cliff       
+            elseif (mod(s, 4) == 2 && a == 3 && s != 2 && s != 46) ||
+                   (s == 1 && a == 2) # cliff       
                 mdp.trans_probs[a, s] = SparseVector(ns, [1], [1.])
                 mdp.reward[a, s] = cliffreward
             else
@@ -33,9 +33,11 @@ function CliffWalkingMDP(; offset = 0.,
             end
         end
     end
-    mdp.reward[3, 46] = goalreward
+    mdp.reward[3, 45] = goalreward
     mdp.reward .+= offset
-    mdp
+    m = ones(Int64, 4, 12)
+    m[5:4:41] = 0
+    DiscreteMaze(mdp, m, [45], collect(1:48))
 end
 
-export CliffWalkingMDP
+export CliffWalking
