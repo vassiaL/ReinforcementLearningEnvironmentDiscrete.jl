c42f4e325d85e939445ba3d416c8b085689784df
VL <vasiliki.liakoni@epfl.ch>
2020-07-15 09:06:32 +0200

    Small fixes

--- src/maze.jl
+++ src/maze.jl
@@ -411,7 +411,7 @@ function interact!(env::ChangeDiscreteMazeProbabilistic, action)
     r = rand(env.rng)
 
     if r < env.changeprobability # Switch or not!
-        println("Switch!")
+        # println("Switch!")
         # @show env.switchflag[:, 76]
         # @show env.discretemaze.mdp.trans_probs[1, 76]
         nswitches = length(env.switchpos)
